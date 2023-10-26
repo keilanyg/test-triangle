db4cce4bafd0293c2465dc654ca23a56668edbd6
keilany <keilanydes@gmail.com>
2023-10-26 17:01:30 -0300

    Alterações e edições nos casos de testes e adição da função "Cada lado deve ser menor que a soma de todos os lados dividida por 2"

--- src/triangulopast/triangulo.py
+++ src/triangulopast/triangulo.py
@@ -4,15 +4,19 @@ class Triangulo:
     def __lado_Coprimento_0(self, a, b, c):
         if (a != b != c != 0):
             return True 
-        return False
-
+            
+    #Cada lado deve ser menor que a soma de todos os lados dividida por 2;
+    def __ladoMenor_SomadosLados_Div2(self, a, b, c):
+        soma_lados = a + b + c 
+        if a <  b <  c < ( soma_lados / 2 ):
+            return True 
+           
     #Um lado é igual à soma do outro
-    def __lado_menor_soma_dos_outros_lados(self, a, b, c):
-        if a < b + c and b < a + c and c < a + b:
+    def __ladoIgualSomadoOutro(self, a, b, c):
+        if (a < b + c and b < a + c and c < a + b):
             return True
-        return False
     
     def verificar(self, a, b, c):
-        if self.__lado_menor_soma_dos_outros_lados(a, b, c) and self.__lado_Coprimento_0(a, b, c):
+        if (self.__ladoMenor_SomadosLados_Div2(a, b, c)  and self.__ladoIgualSomadoOutro(a, b, c) and self.__lado_Coprimento_0(a, b, c)):
             return True
         return False
--- testes/test_triangl.py
+++ testes/test_triangl.py
@@ -1,18 +1,26 @@
 from src.triangulopast.triangulo import Triangulo
 
 
-def test_all_values_zero():
+def test_string1():
     t = Triangulo()
     assert t.verificar(0, 0, 0) == False
     
-def test_one_value_zero():
+def test_string2():
     t = Triangulo()
     assert t.verificar(5, 0, 7) == False
     
-def test_sum_of_two_values_equal_to_the_third():
+def test_string3():
+    t = Triangulo()
+    assert t.verificar(30, 40, 50) == True
+    
+def test_string4():
     t = Triangulo()
     assert t.verificar(4, 6, 10) == False
-
-def test_valid_triangle():
+    
+def test_string5():
     t = Triangulo()
     assert t.verificar(5, 6, 8) == True
+
+def test_string6():
+    t = Triangulo()
+    assert t.verificar(9, 8, 16) == False
